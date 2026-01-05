4a37ffde5fb7d6aefb64ec6bc1d7f3ce08fd399f
Thomas Trapp <trapplab@gmx.de>
2026-01-05 16:03:50 +0100

    updated for reproducible builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,8 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.trapplab.nfc_radio"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 34
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -32,12 +32,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.trapplab.nfc_radio"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -56,13 +55,8 @@ android {
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
 
-            isMinifyEnabled = true
-            isShrinkResources = true
-
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
--- update_todos.md
+++ update_todos.md
@@ -1,8 +1,12 @@
 # Increase version
-- [ ] pubspec.yaml
-- [ ] Changelog.md
-- [ ] add Tag
+- [ ] update pubspec.yaml
+- [ ] update Changelog.md
+- [ ] add Tag in git
 
 # call fastlane
 - [ ] update screenshots
-- [ ] call update_changelogs.sh to update fastlane changelogs from Changelog.md (shoudl be challed automatically by a post commit hook)
+- [ ] update fastlane changelogs from Changelog.md (shoudl be challed automatically by a post commit hook)
+
+# in fdroid data
+- [ ] run: fdroid lint com.trapplab.nfc_radio
+- [ ] run: fdroid rewritemeta com.trapplab.nfc_radio
